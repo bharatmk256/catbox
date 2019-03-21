bc1ce38664ab4490018b44cddf91c2fb9bf62116
bharatmk257 <bharatmk257@gmail.com>
2019-03-21 16:16:59 +0530

    project is about to finish but now i am adding firebase instade to json

---
+++ lib/models/cat.dart
@@ -0,0 +1,29 @@
+import 'package:meta/meta.dart';
+
+class Cat {
+  final int externalId;
+  final String name;
+  final String description;
+  final String avatarUrl;
+  final String location;
+  final int likeCouner;
+  final bool isAdopted;
+  final List<String> pictures;
+  final List<String> cattributes;
+
+  Cat({
+    @required this.externalId,
+    @required this.name,
+    @required this.description,
+    @required this.avatarUrl,
+    @required this.location,
+    @required this.likeCouner,
+    @required this.isAdopted,
+    @required this.pictures,
+    @required this.cattributes,
+  });
+  @override
+  String toString() {
+    return 'Cat $externalId is name $name';
+  }
+}
